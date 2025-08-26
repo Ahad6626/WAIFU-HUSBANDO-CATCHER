69c4e2d5e04aa1922583e8b4e7c822c618facab0
Ahad6626 <asad575530@gmail.com>
2025-08-26 17:42:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,15 +5,15 @@ class Config(object):
     OWNER_ID = "8195241636"
     sudo_users = "8195241636", "8195241636"
     GROUP_ID = -1002847095020
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "8319779558:AAEgds2ieL21JU-4J0nZhOJAz2_RfLi65nY"
+    mongo_url = "mongodb+srv://ahaan:ahaad@ahaan.hgkeruq.mongodb.net/?retryWrites=true&w=majority&appName=ahaan"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = 22657083
+    api_hash = "d6186691704bd901bdab275ceaab88f3"
 
     
 class Production(Config):
