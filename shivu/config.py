3847745f08c8ea00a85707e7d9080b9c28b348ef
Ahad6626 <asad575530@gmail.com>
2025-08-26 17:46:01 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,9 +8,9 @@ class Config(object):
     TOKEN = "8319779558:AAEgds2ieL21JU-4J0nZhOJAz2_RfLi65nY"
     mongo_url = "mongodb+srv://ahaan:ahaad@ahaan.hgkeruq.mongodb.net/?retryWrites=true&w=majority&appName=ahaan"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
+    SUPPORT_CHAT = "IvanxNisha"
+    UPDATE_CHAT = "BotsxUpdate"
+    BOT_USERNAME = "PreetixMusic_bot"
     CHARA_CHANNEL_ID = "-1002133191051"
     api_id = 22657083
     api_hash = "d6186691704bd901bdab275ceaab88f3"
